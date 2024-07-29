04849b954291945b6661ce06c5844b31c0c46de0
DavidJones <qowera@gmail.com>
2024-07-29 11:19:22 +0800

    fix: 修复私聊跳过问题

--- src/core/register/wechaty/mention.ts
+++ src/core/register/wechaty/mention.ts
@@ -3,14 +3,8 @@ import { chat, type ChatHandle } from './chat'
 export function mention(handle: ChatHandle) {
   return chat(async (context) => {
     const { content, isRoom, messager, logger } = context
-    if (!isRoom) {
-      logger.debug('Not a room, skip mention.')
-      return
-    }
-
-    const mentionMe = await messager.mentionSelf()
-    if (!mentionMe) {
-      logger.debug('Not mention me, skip mention.')
+    if (isRoom && !(await messager.mentionSelf())) {
+      logger.debug('In chat room, but not mention me, skip mention.')
       return
     }
 
